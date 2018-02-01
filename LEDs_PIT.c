7e6da67d7e32652f488cb2c758795c1c12f97d74
PacoAvelar <ie704217@iteso.mx>
2018-01-31 23:17:12 -0600

    Formato adecuado

--- LEDs_PIT.c
+++ LEDs_PIT.c
@@ -27,7 +27,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 /**
  * @file    LEDs_PIT.c
  * @brief   Application entry point.
@@ -52,7 +52,6 @@
 #define LED_BLUE_CLOCK_PORT kCLOCK_PortB
 #define LED_GREEN_CLOCK_PORT  kCLOCK_PortE
 
-
 #define SW2_PIN 4
 #define SW3_PIN 6
 #define SW2_PORT PORTA
@@ -62,228 +61,197 @@
 #define SW2_INTERRUPT_FLAG_PIN (1 << SW2_PIN)
 #define SW3_INTERRUPT_FLAG_PIN (1 << SW3_PIN)
 
-
-
-enum {LED_ON, LED_OFF};
-enum {FALSE, TRUE};
-typedef struct color {
-	uint8_t led_rojo;
-	uint8_t led_verde;
-	uint8_t led_azul;
-}color;
-
-typedef struct  {
-	uint8_t bandera_congelado;
-	uint8_t bandera_siguiente;
-	struct estado* estado_anterior;
-	struct color color_del_led;
-	struct estado* estado_siguiente;
-	uint8_t cambiar;
-}estado;
-
-enum {COLOR_SIGUIENTE, COLOR_ANTERIOR};
-enum {NO_CONGELADO, CONGELADO};
-static volatile color color_rojo ={
-		LED_ON,
-		LED_OFF,
-		LED_OFF,
+enum
+{
+    LED_ON, LED_OFF
 };
-
-static volatile color color_azul = {
-		LED_OFF,
-		LED_OFF,
-		LED_ON,
+enum
+{
+    FALSE, TRUE
 };
+typedef struct color
+{
+    uint8_t led_rojo;
+    uint8_t led_verde;
+    uint8_t led_azul;
+} color;
 
-static volatile color color_verde ={
-		LED_OFF,
-		LED_ON,
-		LED_OFF,
+typedef struct
+{
+    uint8_t bandera_congelado;
+    uint8_t bandera_siguiente;
+    struct estado* estado_anterior;
+    struct color color_del_led;
+    struct estado* estado_siguiente;
+    uint8_t cambiar;
+} estado;
+
+enum
+{
+    COLOR_SIGUIENTE, COLOR_ANTERIOR
 };
+enum
+{
+    NO_CONGELADO, CONGELADO
+};
+static volatile color color_rojo = { LED_ON, LED_OFF, LED_OFF, };
 
+static volatile color color_azul = { LED_OFF, LED_OFF, LED_ON, };
 
- void encender_led( estado Estado){
-	 GPIO_PinWrite(GPIOB, LED_RED_PIN,  Estado.color_del_led.led_rojo);
-	 GPIO_PinWrite(GPIOB, LED_BLUE_PIN, Estado.color_del_led.led_azul);
-	 GPIO_PinWrite(GPIOE, LED_GREEN_PIN, Estado.color_del_led.led_verde);
- }
+static volatile color color_verde = { LED_OFF, LED_ON, LED_OFF, };
 
- static estado * estado_actual = 0;
+void encender_led(estado Estado)
+{
+    GPIO_PinWrite(GPIOB, LED_RED_PIN, Estado.color_del_led.led_rojo);
+    GPIO_PinWrite(GPIOB, LED_BLUE_PIN, Estado.color_del_led.led_azul);
+    GPIO_PinWrite(GPIOE, LED_GREEN_PIN, Estado.color_del_led.led_verde);
+}
 
+static estado * estado_actual = 0;
 
+void PORTA_IRQHandler()
+{
+    GPIO_ClearPinsInterruptFlags(GPIOA, SW2_INTERRUPT_FLAG_PIN);
+    estado_actual->bandera_congelado = CONGELADO;
+}
 
- void PORTA_IRQHandler(){
- 	GPIO_ClearPinsInterruptFlags(GPIOA, SW2_INTERRUPT_FLAG_PIN);
- 	estado_actual->bandera_congelado = CONGELADO;
- }
+void PORTC_IRQHandler()
+{
+    GPIO_ClearPinsInterruptFlags(GPIOC, SW3_INTERRUPT_FLAG_PIN);
+    if (COLOR_SIGUIENTE == estado_actual->bandera_siguiente)
+    {
+        estado_actual->bandera_siguiente = COLOR_ANTERIOR;
+    } else
+    {
+        estado_actual->bandera_siguiente = COLOR_SIGUIENTE;
+    }
 
- void PORTC_IRQHandler(){
- 	GPIO_ClearPinsInterruptFlags(GPIOC, SW3_INTERRUPT_FLAG_PIN);
- 	if(COLOR_SIGUIENTE == estado_actual->bandera_siguiente){
- 			estado_actual->bandera_siguiente = COLOR_ANTERIOR;
- 	}else{
- 		estado_actual->bandera_siguiente = COLOR_SIGUIENTE;
- 	}
+}
 
- }
+void PIT0_IRQHandler()
+{
+    PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
+    estado_actual->cambiar = TRUE;
+    PIT_StartTimer(PIT, kPIT_Chnl_0);
+}
 
- void PIT0_IRQHandler (){
-	 PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
-	 estado_actual->cambiar = TRUE;
-	 PIT_StartTimer(PIT, kPIT_Chnl_0);
- }
+int main(void)
+{
+    estado estado_rojo;
 
-int main(void) {
-	 estado estado_rojo ;
+    estado estado_azul;
 
-	 estado estado_azul;
+    estado estado_verde;
 
-	 estado estado_verde;
+    estado_rojo.bandera_congelado = NO_CONGELADO;
+    estado_rojo.bandera_siguiente = COLOR_SIGUIENTE;
+    estado_rojo.estado_anterior = &estado_verde;
+    estado_rojo.color_del_led = color_rojo;
+    estado_rojo.estado_siguiente = &estado_azul;
+    estado_rojo.cambiar = FALSE;
 
+    estado_azul.bandera_congelado = NO_CONGELADO;
+    estado_azul.bandera_siguiente = COLOR_SIGUIENTE;
+    estado_azul.estado_anterior = &estado_rojo;
+    estado_azul.color_del_led = color_azul;
+    estado_azul.estado_siguiente = &estado_verde;
+    estado_azul.cambiar = FALSE;
 
+    estado_verde.bandera_congelado = NO_CONGELADO;
+    estado_verde.bandera_siguiente = COLOR_SIGUIENTE;
+    estado_verde.estado_anterior = &estado_azul;
+    estado_verde.color_del_led = color_verde;
+    estado_verde.estado_siguiente = &estado_rojo;
+    estado_verde.cambiar = FALSE;
 
-
-	estado_rojo.bandera_congelado = NO_CONGELADO;
-	estado_rojo.bandera_siguiente =  COLOR_SIGUIENTE;
-	estado_rojo.estado_anterior =	&estado_verde;
-	estado_rojo.color_del_led =		color_rojo;
-	estado_rojo.estado_siguiente =	&estado_azul;
-	estado_rojo.cambiar = FALSE;
-
-	estado_azul.bandera_congelado = NO_CONGELADO;
-	estado_azul.bandera_siguiente =  COLOR_SIGUIENTE;
-	estado_azul.estado_anterior =	&estado_rojo;
-	estado_azul.color_del_led =		color_azul;
-	estado_azul.estado_siguiente =	&estado_verde;
-	estado_azul.cambiar = FALSE;
-
-
-	estado_verde.bandera_congelado = NO_CONGELADO;
-	estado_verde.bandera_siguiente =  COLOR_SIGUIENTE;
-	estado_verde.estado_anterior =	&estado_azul;
-	estado_verde.color_del_led =		color_verde;
-	estado_verde.estado_siguiente =	&estado_rojo;
-	estado_verde.cambiar = FALSE;
-
-	estado_actual = &estado_rojo;
-  	/* Init board hardware. */
+    estado_actual = &estado_rojo;
+    /* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
+    /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 
-/*
- * Se habilitan los relojes de cada uno de los periféricos.
- */
-   CLOCK_EnableClock(LED_RED_CLOCK_PORT);
-   CLOCK_EnableClock(LED_BLUE_CLOCK_PORT);
-   CLOCK_EnableClock(LED_GREEN_CLOCK_PORT);
+    /*
+     * Se habilitan los relojes de cada uno de los periféricos.
+     */
+    CLOCK_EnableClock(LED_RED_CLOCK_PORT);
+    CLOCK_EnableClock(LED_BLUE_CLOCK_PORT);
+    CLOCK_EnableClock(LED_GREEN_CLOCK_PORT);
 
-   /* Output pin PORT configuration*/
-   port_pin_config_t config_blue_led = {
-		   kPORT_PullDisable,
-       kPORT_FastSlewRate,
-       kPORT_PassiveFilterDisable,
-       kPORT_OpenDrainDisable,
-       kPORT_LowDriveStrength,
-       kPORT_MuxAsGpio,
-       kPORT_UnlockRegister,
-   };
+    /* Output pin PORT configuration*/
+    port_pin_config_t config_blue_led = { kPORT_PullDisable, kPORT_FastSlewRate,
+            kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+            kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister, };
 
-   port_pin_config_t config_red_led = {
-		   kPORT_PullDisable,
-       kPORT_FastSlewRate,
-       kPORT_PassiveFilterDisable,
-       kPORT_OpenDrainDisable,
-       kPORT_LowDriveStrength,
-       kPORT_MuxAsGpio,
-       kPORT_UnlockRegister,
-   };
+    port_pin_config_t config_red_led = { kPORT_PullDisable, kPORT_FastSlewRate,
+            kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+            kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister, };
 
+    port_pin_config_t config_green_led = { kPORT_PullDisable,
+            kPORT_FastSlewRate, kPORT_PassiveFilterDisable,
+            kPORT_OpenDrainDisable, kPORT_LowDriveStrength, kPORT_MuxAsGpio,
+            kPORT_UnlockRegister, };
 
-   port_pin_config_t config_green_led = {
-  		   kPORT_PullDisable,
-         kPORT_FastSlewRate,
-         kPORT_PassiveFilterDisable,
-         kPORT_OpenDrainDisable,
-         kPORT_LowDriveStrength,
-         kPORT_MuxAsGpio,
-         kPORT_UnlockRegister,
-     };
+    /* Sets the configuration*/
+    PORT_SetPinConfig(LED_BLUE_PORT, LED_BLUE_PIN, &config_blue_led);
+    PORT_SetPinConfig(LED_RED_PORT, LED_RED_PIN, &config_red_led);
+    PORT_SetPinConfig(LED_GREEN_PORT, LED_GREEN_PIN, &config_green_led);
 
-   /* Sets the configuration*/
-   PORT_SetPinConfig(LED_BLUE_PORT, LED_BLUE_PIN, &config_blue_led);
-   PORT_SetPinConfig(LED_RED_PORT, LED_RED_PIN, &config_red_led);
-   PORT_SetPinConfig(LED_GREEN_PORT, LED_GREEN_PIN, &config_green_led);
+    /*
+     * Configuración de los botones.
+     */
 
+    CLOCK_EnableClock(SW2_CLOCK_PORT);
+    CLOCK_EnableClock(SW3_CLOCK_PORT);
 
+    port_pin_config_t config_SW3 = { kPORT_PullDisable, kPORT_FastSlewRate,
+            kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+            kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister, };
+    port_pin_config_t config_SW2 = { kPORT_PullDisable, kPORT_FastSlewRate,
+            kPORT_PassiveFilterDisable, kPORT_OpenDrainDisable,
+            kPORT_LowDriveStrength, kPORT_MuxAsGpio, kPORT_UnlockRegister, };
 
+    PORT_SetPinConfig(SW2_PORT, SW2_PIN, &config_SW2);
+    PORT_SetPinConfig(SW3_PORT, SW3_PIN, &config_SW3);
 
-   /*
-    * Configuración de los botones.
-    */
+    /*
+     * Congifuracion del PIT 0
+     */
 
-   CLOCK_EnableClock(SW2_CLOCK_PORT);
-   CLOCK_EnableClock(SW3_CLOCK_PORT);
+    pit_config_t config_pit_0;
+    PIT_GetDefaultConfig(&config_pit_0);
+    CLOCK_EnableClock(kCLOCK_Pit0);
+    PIT_Init(PIT, &config_pit_0);
+    uint32_t clock_fq = CLOCK_GetFreq(kCLOCK_Pit0);
+    uint8_t seconds = 1;
+    uint32_t pit_0_clock_counts = ((uint32_t) seconds) * clock_fq;
+    PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, pit_0_clock_counts);
+    PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+    PIT_StartTimer(PIT, kPIT_Chnl_0);
+    EnableIRQ(PIT0_IRQn);
 
-   port_pin_config_t config_SW3 = {
-  		   kPORT_PullDisable,
-         kPORT_FastSlewRate,
-         kPORT_PassiveFilterDisable,
-         kPORT_OpenDrainDisable,
-         kPORT_LowDriveStrength,
-         kPORT_MuxAsGpio,
-         kPORT_UnlockRegister,
-     };
-   port_pin_config_t config_SW2 = {
-  		   kPORT_PullDisable,
-         kPORT_FastSlewRate,
-         kPORT_PassiveFilterDisable,
-         kPORT_OpenDrainDisable,
-         kPORT_LowDriveStrength,
-         kPORT_MuxAsGpio,
-         kPORT_UnlockRegister,
-     };
+    NVIC_EnableIRQ(PORTA_IRQn);
+    NVIC_EnableIRQ(PORTC_IRQn);
 
-   PORT_SetPinConfig(SW2_PORT, SW2_PIN, &config_SW2);
-   PORT_SetPinConfig(SW3_PORT, SW3_PIN, &config_SW3);
-
-   /*
-    * Congifuracion del PIT 0
-    */
-
-   pit_config_t config_pit_0;
-   PIT_GetDefaultConfig(&config_pit_0);
-   CLOCK_EnableClock(kCLOCK_Pit0);
-   PIT_Init(PIT, &config_pit_0);
-   uint32_t clock_fq = CLOCK_GetFreq(kCLOCK_Pit0);
-   uint8_t seconds = 1;
-   uint32_t pit_0_clock_counts = ((uint32_t) seconds ) * clock_fq;
-   PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, pit_0_clock_counts);
-   PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
-   PIT_StartTimer(PIT, kPIT_Chnl_0);
-   EnableIRQ(PIT0_IRQn);
-
-	NVIC_EnableIRQ(PORTA_IRQn);
-	NVIC_EnableIRQ(PORTC_IRQn);
-
-
-    for(;;)	{
-    	if(TRUE == estado_actual->cambiar){
-    		if(NO_CONGELADO == estado_actual->bandera_congelado){
-    		    		if(COLOR_SIGUIENTE == estado_actual->bandera_siguiente){
-    		    			estado_actual = estado_actual->estado_siguiente;
-    		    		}else{
-    		    			estado_actual = estado_actual->estado_anterior;
-    		    		}
-    		    		encender_led(*estado_actual);
-    		    	}
-    		estado_actual->cambiar = FALSE;
-    	}
-
-
+    for (;;)
+    {
+        if (TRUE == estado_actual->cambiar)
+        {
+            if (NO_CONGELADO == estado_actual->bandera_congelado)
+            {
+                if (COLOR_SIGUIENTE == estado_actual->bandera_siguiente)
+                {
+                    estado_actual = estado_actual->estado_siguiente;
+                } else
+                {
+                    estado_actual = estado_actual->estado_anterior;
+                }
+                encender_led(*estado_actual);
+            }
+            estado_actual->cambiar = FALSE;
+        }
 
     }
-    return 0 ;
+    return 0;
 }
